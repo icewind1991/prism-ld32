608f8508471358a8ee8389c2ee4ec52b95aebb7d
Robin Appelman <icewind@owncloud.com>
2015-04-18 20:56:09 +0200

    allow multiple prisms

--- src/bendray.js
+++ src/bendray.js
@@ -2,37 +2,48 @@ var PIXI = require('pixi.js');
 var Ray = require('./ray');
 
 class BendRay extends Ray {
-	constructor(origin, direction, prism, color, refractionScale) {
+	constructor(origin, direction, prisms, color, refractionScale) {
 		super(origin, direction, color, refractionScale);
-		this.prism = prism;
+		this.prisms = prisms;
 		this.update();
 	}
 
 	update() {
-		var pieces = this.prism.inputRay(this);
+		var pieces = this.prisms.reduce((pieces, prism, i) => {
+			var output = pieces.pop();
+			//console.log(output);
+			var newPieces = prism.inputRay(output);
+			//console.log(newPieces);
+			if (newPieces.length === 2 && i == 1) {
+				//a = 1;
+			}
+			return pieces.concat(newPieces);
+		}, [this]);
+		//var prism = this.prisms[0];
+		//var pieces = prism.inputRay(this);
 		this.clear();
 		this.blendMode = PIXI.blendModes.ADD;
 		this.lineStyle(2, this.color, 1);
-		
+
 		this.moveTo(this.origin[0], this.origin[1]);
 		pieces.forEach((piece) => {
 			if (piece.end) {
 				this.lineTo(piece.end[0], piece.end[1]);
 			} else {
 				/*this.lineTo(piece.origin[0] + piece.direction[0] * 1000,
-					piece.origin[1] + piece.direction[1] * 1000);*/
+				 piece.origin[1] + piece.direction[1] * 1000);*/
 				this.lineStyle(2, 0xFFFFFF, 0);
 				this.beginFill(this.color);
-				
+
 				this.moveTo(piece.origin[0], piece.origin[1]);
 				var angle = piece.angle;
-				var minAngle = angle-Math.PI/128;
-				var maxAngle = angle+Math.PI/128;
+				var minAngle = angle - Math.PI / 128;
+				var maxAngle = angle + Math.PI / 128;
 				var minDir = [Math.cos(minAngle), Math.sin(minAngle)];
 				var maxDir = [Math.cos(maxAngle), Math.sin(maxAngle)];
 				this.lineTo(piece.origin[0] - minDir[0] * 1000, piece.origin[1] - minDir[1] * 1000);
 				this.lineTo(piece.origin[0] - maxDir[0] * 1000, piece.origin[1] - maxDir[1] * 1000);
-				
+
 				this.endFill();
 			}
 		});
--- src/main.js
+++ src/main.js
@@ -13,71 +13,76 @@ document.body.appendChild(renderer.view);
 
 var stage = new PIXI.Stage;
 
-var prism = new Prism();
-prism.position.x = 150;
-prism.position.y = 200;
+var prism1 = new Prism();
+prism1.position.x = 150;
+prism1.position.y = 200;
 
-stage.addChild(prism);
+var prism2 = new Prism();
+prism2.position.x = 550;
+prism2.position.y = 500;
+
+stage.addChild(prism1);
+stage.addChild(prism2);
 var rays = [];
-rays.push(new BendRay([0, renderer.view.height/4], [1, 1], prism, 0xFF0000, 1));//720nm
-rays.push(new BendRay([0, renderer.view.height/4], [1, 1], prism, 0xFF9B00, 0.975));//610nm
-rays.push(new BendRay([0, renderer.view.height/4], [1, 1], prism, 0xFFFF00, 0.95));//580nm
-rays.push(new BendRay([0, renderer.view.height/4], [1, 1], prism, 0x00FF00, 0.925));//510nm
-rays.push(new BendRay([0, renderer.view.height/4], [1, 1], prism, 0x0000FF, 0.9));//440nm
-rays.forEach((ray)=>{
+rays.push(new BendRay([0, renderer.view.height / 4], [1, 1], [prism1, prism2], 0xFF0000, 1));//720nm
+rays.push(new BendRay([0, renderer.view.height / 4], [1, 1], [prism1, prism2], 0xFF9B00, 0.975));//610nm
+rays.push(new BendRay([0, renderer.view.height / 4], [1, 1], [prism1, prism2], 0xFFFF00, 0.95));//580nm
+rays.push(new BendRay([0, renderer.view.height / 4], [1, 1], [prism1, prism2], 0x00FF00, 0.925));//510nm
+rays.push(new BendRay([0, renderer.view.height / 4], [1, 1], [prism1, prism2], 0x0000FF, 0.9));//440nm
+rays.forEach((ray)=> {
 	stage.addChild(ray);
 });
 requestAnimationFrame(animate);
 
 kd.A.down(() => {
-	prism.position.x -= 1;
+	prism1.position.x -= 1;
 });
 
 kd.S.down(() => {
-	prism.position.y += 1;
+	prism1.position.y += 1;
 });
 
 kd.D.down(() => {
-	prism.position.x += 1;
+	prism1.position.x += 1;
 });
 
 kd.W.down(() => {
-	prism.position.y -= 1;
+	prism1.position.y -= 1;
 });
 
 kd.Q.down(() => {
-	prism.rotation -= 0.01;
+	prism1.rotation -= 0.01;
 });
 
 kd.E.down(() => {
-	prism.rotation += 0.01;
+	prism1.rotation += 0.01;
 });
 
 kd.R.down(() => {
-	prism.refractionIndex += 0.01;
-	if(prism.refractionIndex > 2.5) {
-		prism.refractionIndex = 2.5;
-	}	
+	prism1.refractionIndex += 0.01;
+	if (prism1.refractionIndex > 2.5) {
+		prism1.refractionIndex = 2.5;
+	}
 });
 
 kd.T.down(() => {
-	prism.refractionIndex -= 0.01;
-	if(prism.refractionIndex < 1) {
-		prism.refractionIndex = 1;
-	}	
+	prism1.refractionIndex -= 0.01;
+	if (prism1.refractionIndex < 1) {
+		prism1.refractionIndex = 1;
+	}
 });
 
 function animate() {
 	kd.tick();
-	//prism.rotation += 0.01;
+	//prism1.rotation += 0.01;
 	var mouse = stage.getMousePosition();
-	rays.forEach((ray)=>{
+	rays.forEach((ray)=> {
 		ray.destination = [mouse.x, mouse.y];
 	});
 	//console.log(stage.getMousePosition());
-	rays.forEach((ray)=>{
+	rays.forEach((ray)=> {
 		ray.update();
-	});	
+	});
 	renderer.render(stage);
 
 	requestAnimationFrame(animate);
