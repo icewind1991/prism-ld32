d81d5482c6a703b247477177ddcd3591b5a1bb91
Robin Appelman <icewind@owncloud.com>
2015-04-19 00:40:07 +0200

    prisms in any order

--- src/bendray.js
+++ src/bendray.js
@@ -11,18 +11,43 @@ class BendRay extends Ray {
 	}
 
 	update() {
-		var pieces = this.prisms.reduce((pieces, prism, i) => {
-			var output = pieces.pop();
-			var newPieces = prism.inputRay(output);
-			if(newPieces.length > 1) {
-				this.hitPrism = true;
-			} else {
-				this.hitPrism = false;
+		var pieces = [this];
+		var prisms = this.prisms.slice(0); //clone
+		var piece = pieces[pieces.length - 1];
+		prisms.sort((a, b) => {
+			var distanceA = Math.pow(a.position.x - piece.origin[0], 2) + Math.pow(a.position.y - piece.origin[1], 2);
+			var distanceB = Math.pow(b.position.x - piece.origin[0], 2) + Math.pow(b.position.y - piece.origin[1], 2);
+			return distanceA - distanceB;
+		});
+		while (prisms.length) {
+			var prism = prisms.shift();
+			var inputPiece = pieces.pop();
+			var newPieces = prism.inputRay(inputPiece);
+			if (newPieces.length > 1) {
+				// retry all prisms on hit
+				prisms = this.prisms.slice(0);
+				piece = newPieces[newPieces.length - 1];
+				prisms.sort((a, b) => {
+					var distanceA = Math.pow(a.position.x - piece.origin[0], 2) + Math.pow(a.position.y - piece.origin[1], 2);
+					var distanceB = Math.pow(b.position.x - piece.origin[0], 2) + Math.pow(b.position.y - piece.origin[1], 2);
+					return distanceA - distanceB;
+				});
 			}
-			return pieces.concat(newPieces);
-		}, [this]);
-		//var prism = this.prisms[0];
-		//var pieces = prism.inputRay(this);
+			pieces = pieces.concat(newPieces);
+		}
+		//var newPieces = prism.inputRay(output);
+		//if (newPieces.length > 0) {
+		//
+		//}
+		//var pieces = this.prisms.reduce((pieces, prism, i) => {
+		//	var output = pieces.pop();
+		//	var newPieces = prism.inputRay(output);
+		//
+		//	return pieces.concat(newPieces);
+		//}, [this]);
+
+
+		this.hitPrism = (pieces.length > 1);
 		this.clear();
 		this.blendMode = PIXI.blendModes.ADD;
 		this.lineStyle(2, this.color, 1);
@@ -32,8 +57,6 @@ class BendRay extends Ray {
 			if (piece.end) {
 				this.lineTo(piece.end[0], piece.end[1]);
 			} else {
-				/*this.lineTo(piece.origin[0] + piece.direction[0] * 1000,
-				 piece.origin[1] + piece.direction[1] * 1000);*/
 				this.lineStyle(2, 0xFFFFFF, 0);
 				this.beginFill(this.color);
 				var angle = piece.angle;
@@ -41,27 +64,27 @@ class BendRay extends Ray {
 				var maxAngle = angle + Math.PI / 128;
 				var minDir = [Math.cos(minAngle), Math.sin(minAngle)];
 				var maxDir = [Math.cos(maxAngle), Math.sin(maxAngle)];
-				this.lastCone = [piece.origin[0], piece.origin[1], 
-								piece.origin[0] - minDir[0] * 1000, piece.origin[1] - minDir[1] * 1000,
-								piece.origin[0] - maxDir[0] * 1000, piece.origin[1] - maxDir[1] * 1000];
+				this.lastCone = [piece.origin[0], piece.origin[1],
+					piece.origin[0] - minDir[0] * 1000, piece.origin[1] - minDir[1] * 1000,
+					piece.origin[0] - maxDir[0] * 1000, piece.origin[1] - maxDir[1] * 1000];
 
 				this.moveTo(this.lastCone[0], this.lastCone[1]);
 				this.lineTo(this.lastCone[2], this.lastCone[3]);
 				this.lineTo(this.lastCone[4], this.lastCone[5]);
-				
+
 				this.endFill();
 			}
 		});
 	}
-	
+
 	get currentCone() {
 		return this.lastCone;
 	}
-	
+
 	get getcolor() {
 		return super.color;
 	}
-	
+
 	get length() {
 		return Math.sqrt(Math.pow(this.end[0] - this.origin[0], 2) + Math.pow(this.end[1] - this.origin[1], 2));
 	}
