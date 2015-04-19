ab4a448b692fc13793db4482c911752ac2d7ab5f
Robin Appelman <icewind@owncloud.com>
2015-04-19 18:04:28 +0200

    combine level

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ var levels = {
 	2: require('../levels/tutorials/mirrors.json'),
 	3: require('../levels/tutorials/filters.json'),
 	4: require('../levels/tutorials/rotation.json'),
-	5: require('../levels/tutorials/all.json')
+	5: require('../levels/tutorials/combine.json')
 };
 
 stage.activeLevel = null;
